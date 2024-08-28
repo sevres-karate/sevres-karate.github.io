6f1884108afc07db5cf097e6401110428efcee40
Frederic Meurou <frederic.meurou@thinkproject.com>
2024-08-28 23:40:13 +0200

    Initial commit

---
+++ generation/pelicanconf.py
@@ -0,0 +1,33 @@
+AUTHOR = 'Sèvres Karaté'
+SITENAME = 'Sèvres Karaté'
+SITEURL = "https://www.sevres-karate.club"
+
+PATH = "../source"
+OUTPUT_PATH = "../"
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = 'fr'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (
+    ("You can modify those links in your config file", "#"),
+)
+
+# Social widget
+SOCIAL = (
+    ("You can add links in your config file", "#"),
+    ("Another social link", "#"),
+)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
