e69922ec04360c73d9c04bcde8679c719a0bd27c
sevres-karate <sevres.karate@gmail.com>
2024-08-29 07:42:26 +0200

    Update publishconf.py

--- publishconf.py
+++ publishconf.py
@@ -9,6 +9,7 @@ from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
 SITEURL = "https://www.sevres-karate.club"
+PATH = "content"
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = "feeds/all.atom.xml"
