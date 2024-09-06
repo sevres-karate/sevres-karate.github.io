42ba52881021b1b16fea0ff1ad3b99aa3164db33
sevres-karate <sevres.karate@gmail.com>
2024-09-06 23:33:21 +0200

    Ajout lien plaquette

--- pelicanconf.py
+++ pelicanconf.py
@@ -24,6 +24,7 @@ ARTICLE_ORDER_BY = "basename"
 LINKS = (
     ("Inscription en ligne", "https://www.helloasso.com/associations/co-sevres-karate/adhesions/inscription-2024-2025"),
     ("Fiche d'inscription 2024-2025", "https://sevres-karate.fr/pdfs/fiche_inscription_coskarate_2024-2025.pdf"),
+    ("Plaquette 2024-2025", "https://sevres-karate.fr/pdfs/plaquette_2024-2025.pdf"),
     ("Federation Française de Karaté et Disciplines Associées", "http://www.ffkama.fr/"),
     ("Ligue des Hauts-de-Seine de Karaté", "http://www.ffkarate.fr/liguehautsdeseine/"),
     ("Japan Karate Association", "http://www.jka.or.jp/"),
