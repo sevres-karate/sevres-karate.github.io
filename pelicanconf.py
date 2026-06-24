c8cfd99b3947d5ceea9e7b4d738efe8595b33d54
sevres-karate <sevres.karate@gmail.com>
2026-06-24 11:36:16 +0200

    Update pelicanconf.py

--- pelicanconf.py
+++ pelicanconf.py
@@ -24,7 +24,7 @@ DEFAULT_DATE_ORDER = "reversed"
 # Blogroll
 LINKS = (
     ("Inscription en ligne", "https://www.helloasso.com/associations/co-sevres-karate/adhesions/inscription-2026-2027"),
-    ("Fiche d'inscription 2025-2026", "https://sevres-karate.fr/pdfs/fiche_inscription_coskarate_2026_2027.pdf"),
+    ("Fiche d'inscription 2026-2027", "https://sevres-karate.fr/pdfs/fiche_inscription_coskarate_2026_2027.pdf"),
     ("Plaquette 2025-2026", "https://sevres-karate.fr/pdfs/plaquette_2025-2026.pdf"),
     ("Federation Française de Karaté et Disciplines Associées", "http://www.ffkama.fr/"),
     ("Ligue des Hauts-de-Seine de Karaté", "http://www.ffkarate.fr/liguehautsdeseine/"),
